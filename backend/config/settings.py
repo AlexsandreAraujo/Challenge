974f360500c365da0ed7f6d4c5c2fbd918dd31e1
Alexsandre <alexsandre.lima.araujo@outlook.com>
2026-09-04 15:24:51 -0300

    chore: cria apps catalogo, pessoas, vendas e comissoes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "catalogo",
+    "pessoas",
+    "vendas",
+    "comissoes",
 ]
 
 MIDDLEWARE = [
