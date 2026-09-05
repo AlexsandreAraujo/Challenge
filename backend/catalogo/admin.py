c4d49a2a14cc78ce13e38c1a680e1994cff60fdc
Alexsandre <alexsandre.lima.araujo@outlook.com>
2026-09-05 10:50:23 -0300

    feat: adiciona model e admin de Produto

--- backend/catalogo/admin.py
+++ backend/catalogo/admin.py
@@ -1,1 +1,13 @@
 """Administração do app catalogo."""
+
+from django.contrib import admin
+
+from .models import Produto
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    """Configuração do Django Admin para o model Produto."""
+
+    list_display = ("codigo", "descricao", "valor_unitario", "percentual_comissao")
+    search_fields = ("codigo", "descricao")
