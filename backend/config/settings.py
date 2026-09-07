df2573f9b88705e6fafa0bca3d44307138e1e1b7
Alexsandre <alexsandre.lima.araujo@outlook.com>
2026-09-07 18:06:54 -0300

    fix: adiciona CSRF_TRUSTED_ORIGINS e SECURE_PROXY_SSL_HEADER para funcionar atras do nginx

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -145,3 +145,6 @@ MAILERS = {
 CORS_ALLOWED_ORIGINS = env.list(
     "CORS_ALLOWED_ORIGINS", default=["http://localhost:5173"]
 )
+
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
